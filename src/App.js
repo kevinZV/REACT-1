e703eed31132f8f2de84642239659bd43e3a6b07
kevinZV <35600973+kevinZV@users.noreply.github.com>
2018-01-31 21:26:00 +0100

    avec commentaire

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//création d'un constructor
 class App extends React.Component {
     constructor() {
         super();
+        //on créer des tableau vide pour les séries et les épisodes qui se rempliront
         this.state = {
             seriesList: [],
             seriesEpisodesList: []
@@ -21,8 +22,9 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-
+        //on recherche les series
         fetch('seriesList.json', {})
+            //promesse d'afficher une réponse
             .then(response => response.json())
             .then(seriesListDepuisFichier => {
                 this.setState({seriesList: seriesListDepuisFichier});
@@ -34,8 +36,9 @@ class App extends React.Component {
             .then(function () {
                 /* alert("j'ai fait ce que j'ai pu");*/
             });
-
+        //on recherche les épiosdes des séries
         fetch('seriesEpisodesList.json', {})
+            //promesse d'afficher une réponse
             .then(response => response.json())
             .then(seriesListDepuisFichier => {
                 this.setState({seriesEpisodesList: seriesListDepuisFichier});
@@ -46,36 +49,39 @@ class App extends React.Component {
             })
 
     }
-
+    //on effectue un rendu
     render() {
         return (
             <div class="wrapper">
-                <h1>Catalogue de série</h1>
-                <input id="recherche" type="text" placeholder="Recherche une serie" value={this.state.value}
-                       onChange={this.handleChange}/>
-                <ul>
-                    {this.state.value !== "" ?
+                <div id="header">
+                    <h1>Catalogue de série</h1>
+                    <input id="recherche" type="text" placeholder="Recherche une serie" value={this.state.value}
+                           onChange={this.handleChange}/>
+                    <ul>
+                        {this.state.value !== "" ?
+                            //fonction pour afficher les séries sous forme de liste
+                            this.state.seriesList.filter(
+                                e => e.seriesName.indexOf(this.state.value) > -1).map(item => <li
+                                key={item.id}>{item.seriesName}
 
-                        this.state.seriesList.filter(
-                            e => e.seriesName.indexOf(this.state.value) > -1).map(item => <li
-                            key={item.id}>{item.seriesName}
+                                <ul>
+                                    //on recupere les episodes en fonction de l'id de la série
+                                    {this.state.seriesEpisodesList.filter(
+                                        f => f.serie_id == item.id).map(episode => episode.episodes_list.filter(
+                                        g => g.episodeName).map(name => <li>{name.episodeName}</li>)
+                                    )
 
-                            <ul>
-                                {this.state.seriesEpisodesList.filter(
-                                    f => f.serie_id == item.id).map(episode => episode.episodes_list.filter(
-                                    g => g.episodeName).map(name => <li>{name.episodeName}</li>)
-                                )
+                                    }
+                                </ul>
+                            </li>)
 
-                                }
-                            </ul>
-                        </li>)
+                            //quand plus rien n'est marque
+                            : <p>Rien n'est Marqué</p>
 
+                        }
 
-                        : <p>Rien n'est Marqué</p>
-
-                    }
-
-                </ul>
+                    </ul>
+                </div>
 
             </div>
         )
